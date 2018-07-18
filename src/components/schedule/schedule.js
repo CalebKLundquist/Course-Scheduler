6c34061c7281a23fd9b32a4296c2b7c709e9504c
Caleb Lundquist <CalebKLundquist@gmail.com>
2018-07-18 19:39:38 +0300

    built library and shedule components and added to grid

---
+++ src/components/schedule/schedule.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Schedule extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="schedule__title">My Schedule</h1>
+            </div>
+        );
+    }
+}
